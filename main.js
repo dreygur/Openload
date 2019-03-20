6feb28d1ffd46cb40aa8aa5b41aa7217cd627aa1
Rakibul Yeasin <rytotul@gmail.com>
2019-03-20 20:32:35 +0600

    Fixed Bug :fire:

--- main.js
+++ main.js
@@ -9,36 +9,36 @@ const night = new Nightmare();
 const port = process.env.PORT || 3000;
 
 var router = express.Router();
-router.get('/', (req, res) => res.json({ message: 'hooray! welcome to our api!' }));
+router.get('/', (req, res) => res.json({ message: 'Welcome User!' }));
 router.route('/:id').get( (req, res) => {
   let url = 'https://openload.co/f/' + req.params.id;
 
   async function getID() {
-    return await night.goto(url)
-                //.wait('#lqEH1')
-                .evaluate(() => {
-                    return document.getElementById('lqEH1').innerHTML;
-                })
-                .end()
-                .then((data) => {
-                    // console.log(data);
-                    return data;
-                })
-                .catch(error => console.error('Search failed:', error));
+    return await night
+    .goto(url)
+    //.wait('#lqEH1')
+    .evaluate(() => {
+      return document.getElementById('lqEH1').innerHTML;
+    })
+    .end()
+    .then((data) => {
+      // console.log(data);
+      return data;
+    })
+    .catch(error => console.error('Search failed:', error));
   }
 
   const streamLink = async id => {
-      let data = await getID();
-      let stream = `https://openload.co/stream/${data}?mime=true`
-      console.log(stream);
-      res.send(`Link: ${stream}`);
-      //return data;
+    let data = await getID();
+    let stream = `https://openload.co/stream/${data}?mime=true`
+    console.log(stream);
+    res.json({Link: `${stream}`});
+    //return data;
   };
-
   streamLink();
-
   //res.send(`ID: ${req.params.id}`);
 });
+
 app.use('/', router);
 
 //app.get('/', (req, res) => res.send('Hello World!'));
