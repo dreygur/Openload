75fb2b2cb160f5b7a7781da85e0f420544b7ec82
Rakibul Yeasin <rytotul@gmail.com>
2019-03-20 16:28:08 +0600

    Initialized

---
+++ index.js
@@ -0,0 +1,34 @@
+/* jshint esversion: 6 */
+
+const Nightmare = require('nightmare');
+
+const night = new Nightmare();
+// const night = new Nightmare({waitTimeout: 60000});
+
+let id = 'jTySySdmPhU';
+let url = 'https://openload.co/f/' + id;
+
+let myId;
+
+async function getID() {
+    return await night.goto(url)
+                //.wait('#lqEH1')
+                .evaluate(() => {
+                    return document.getElementById('lqEH1').innerHTML;
+                })
+                .end()
+                .then((data) => {
+                    // console.log(data);
+                    return data;
+                })
+                .catch(error => console.error('Search failed:', error));
+}
+
+const series = async id => {
+    let data = await getID();
+    let stream = `https://openload.co/stream/${data}?mime=true`
+    console.log(stream);
+    //return data;
+};
+
+series();
